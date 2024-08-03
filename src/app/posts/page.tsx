a4d08708e78f46bae896fb82c09b0e74e4431362
Salman-sajib <sfsajib@gmail.com>
2024-08-03 19:20:46 +0600

    fetched the posts data and render it with a loader

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,14 @@
-function Page() {
+import PostsList from "@/components/PostsList";
+import { Suspense } from "react";
+
+async function Page() {
   return (
-    <main className=" text-center pt-32 px-5 ">
-      <h1 className=" text-4xl md:text-5xl font-bold mb-5 ">All posts</h1>
+    <main className=" text-center pt-16 px-5 ">
+      <h1 className=" text-4xl md:text-5xl font-bold mb-5 ">All Posts</h1>
+
+      <Suspense fallback="Loading posts...">
+        <PostsList />
+      </Suspense>
     </main>
   );
 }
